287a90b26ca7bb6c52cf3cd4000909c4297f4fd6
JagannathaReddy <granular007@gmail.com>
2024-03-13 08:29:40 +0530

    package

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 };
